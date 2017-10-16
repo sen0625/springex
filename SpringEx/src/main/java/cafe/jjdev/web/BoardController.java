effb69aef70447314360df0db1cf29e26c8d0f18
201-07 <201-07@201-07-PC>
2017-10-16 10:17:22 +0900

    DB에 입력완료

--- SpringEx/src/main/java/cafe/jjdev/web/BoardController.java
+++ SpringEx/src/main/java/cafe/jjdev/web/BoardController.java
@@ -1,10 +1,28 @@
 package cafe.jjdev.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sun.tracing.dtrace.Attributes;
+
+import cafe.jjdev.web.service.Board;
+import cafe.jjdev.web.service.BoardDao;
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardDao boardDao;
+	
+	// 입력(액션) 요청
+    @RequestMapping(value="/boardAdd", method = RequestMethod.POST)
+    public String boardAdd(Board board) {
+        System.out.println(board);
+        boardDao.insertBoard(board);
+        return "redirect:/boardList"; // 글입력후 "/boardList"로 리다이렉트(재요청)
+    }
+    
     @RequestMapping(value="/boardAdd", method = RequestMethod.GET)
     public String boardAdd() {
         System.out.println("boardAdd 폼 요청");
--- SpringEx/src/main/java/cafe/jjdev/web/service/BoardDao.java
+++ SpringEx/src/main/java/cafe/jjdev/web/service/BoardDao.java
@@ -10,36 +10,39 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class BoardDao {
+	
+	private final String NS = "cafe.jjdev.web.service.BoardMapper.";
+	
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
 
 	public int updateBoard(Board board) {
-		return sqlSessionTemplate.update("cafe.jjdev.board.service.BoardMapper.updateBoard", board);
+		return sqlSessionTemplate.update(NS+"updateBoard", board);
 	}
 
 	public int deleteBoard(int boardNo, String boardPw) {
 		Board board = new Board();
 		board.setBoardNo(boardNo);
 		board.setBoardPw(boardPw);
-		return sqlSessionTemplate.delete("cafe.jjdev.board.service.BoardMapper.deleteBoard", board);
+		return sqlSessionTemplate.delete(NS+"deleteBoard", board);
 	}
 
 	public Board getBoard(int boardNo) {
-		return sqlSessionTemplate.selectOne("cafe.jjdev.board.service.BoardMapper.getBoard", boardNo);
+		return sqlSessionTemplate.selectOne(NS+"getBoard", boardNo);
 	}
 
 	public List<Board> getBoardList(int currentPage, int pagePerRow) {
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		map.put("beginRow", (currentPage - 1) * pagePerRow);
 		map.put("pagePerRow", pagePerRow);
-		return sqlSessionTemplate.selectList("cafe.jjdev.board.service.BoardMapper.getBoardList", map);
+		return sqlSessionTemplate.selectList(NS+"getBoardList", map);
 	}
 
 	public int getBoardCount() {
-		return sqlSessionTemplate.selectOne("cafe.jjdev.board.service.BoardMapper.getBoardCount");
+		return sqlSessionTemplate.selectOne(NS+"getBoardCount");
 	}
 
 	public int insertBoard(Board board) {
-		return sqlSessionTemplate.insert("cafe.jjdev.board.service.BoardMapper.insertBoard", board);
+		return sqlSessionTemplate.insert(NS+"insertBoard", board);
 	}
 }
